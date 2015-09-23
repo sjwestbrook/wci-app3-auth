bcb3b0df8ed9d764c3677b6405ff8382ec16cf96
sjwestbrook <sawest4291@gmail.com>
2015-09-23 11:38:57 -0600

    firebase auth finished

--- app/admin/adminCtrl.js
+++ app/admin/adminCtrl.js
@@ -1,6 +1,6 @@
 var app = angular.module('wciApp');
 
-app.controller('adminCtrl', function($scope, $rootScope, $firebaseAuth, $firebaseObject, $firebaseArray, adminServ){
+app.controller('adminCtrl', function($scope, $firebaseAuth, $firebaseObject, $firebaseArray, adminServ){
 
   adminServ.getUsers().then(function(usersArr){
     $scope.users = usersArr
@@ -18,26 +18,32 @@ app.controller('adminCtrl', function($scope, $rootScope, $firebaseAuth, $firebas
   var ref = new Firebase('https://wci-app.firebaseio.com');
 
   $scope.authObj = $firebaseAuth(ref);
-  // $rootScope.alert = {};
 
   $scope.signIn = function () {
-    $scope.authObj.$authWithPassword({   // $rootScope.auth.$login isn't a function
+    $scope.authObj.$authWithPassword({
       email: $scope.email,
       password: $scope.password
     }).then(function(user) {
       $scope.authData = user;
       console.log('logged in as' + user.uid)
-      //$rootScope.alert.message = '';
     }, function(error) {
-      if (error = 'INVALID_EMAIL') {
-        console.error('Invalid Email');
-      } else if (error = 'INVALID_PASSWORD') {
-        console.error('Invalid Password');
-      } else {
-        console.error(error);
+      if (error) {
+        console.error('Invalid Login');
       }
     });
   }
 
+//------------------------------------------------------------------------------
+
+  $scope.logout = function() {
+    $scope.authData = null;
+
+    // console.log('logged in as' + user.uid);
+      // data disappears; error "user is not defined" in console --> ok?
+
+    // empty email and password inputs, so unauthorized can't sign in
+    $scope.email = null;
+    $scope.password = null;
+  }
 
 });
